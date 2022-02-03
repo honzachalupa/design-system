24b36f8ec688c6ceb5f9506761f2e8e6dab321ee
Jan Chalupa <janchalupa@outlook.cz>
2022-02-03 14:43:55 +0100

    chore: Added reexport of @react-pdf/renderer library.

---
+++ packages/ui/src/components/molecules/PdfRenderer/index.ts
@@ -0,0 +1,17 @@
+import {
+    Document,
+    Font,
+    Page,
+    PDFDownloadLink,
+    Text,
+    View,
+} from "@react-pdf/renderer";
+
+export const PdfRenderer = {
+    Document,
+    Font,
+    Page,
+    Text,
+    View,
+    PDFDownloadLink,
+};
