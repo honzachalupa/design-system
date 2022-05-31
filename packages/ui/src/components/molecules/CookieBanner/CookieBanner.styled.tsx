0739d8f1e3aeffb9aeb57ecac33eb053b969b453
Jan Chalupa <janchalupa@outlook.cz>
2022-05-31 11:17:24 +0200

    fix(CookieBanner)

--- packages/ui/src/components/molecules/CookieBanner/CookieBanner.styled.tsx
+++ packages/ui/src/components/molecules/CookieBanner/CookieBanner.styled.tsx
@@ -4,12 +4,13 @@ import { ButtonsGroup } from "../ButtonsGroup";
 export const StyledContainer = styled.div`
     width: 100%;
     background-color: #eff5f7;
-    position: fixed;
-    bottom: 0;
-    left: 0;
     padding: 15px;
     display: flex;
     justify-content: space-between;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    z-index: 1;
 `;
 
 export const StyledContent = styled.div`
