50f61bcf6ff712f2f1a93e0b6cc3be2d37193489
Jan Chalupa <janchalupa@outlook.cz>
2022-12-01 14:02:48 +0100

    feat(Utils): Added useExceptionHandler hook.

--- packages/utils/src/hooks/useExceptionHandler.ts
+++ packages/utils/src/hooks/useExceptionHandler.ts
@@ -1,14 +1,14 @@
 import useEventListener from "./useEventListener";
 
 interface IProps {
-    onException: (exception: { message: string; scope: string }) => void;
+    onException: (exception: { message: string; stack: string }) => void;
 }
 
 export const useExceptionHandler = ({ onException }: IProps) => {
     useEventListener("error", ({ error }: ErrorEvent) => {
         onException({
             message: error.message,
-            scope: error.scope,
+            stack: error.stack,
         });
     });
 
