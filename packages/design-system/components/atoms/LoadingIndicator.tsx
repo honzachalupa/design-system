594d42afa1faa959e31a678ca7fc4472fdf6d834
Jan Chalupa <janchalupa@outlook.cz>
2023-04-25 16:15:47 +0200

    chore: WIP.

--- packages/design-system/components/atoms/LoadingIndicator.tsx
+++ packages/design-system/components/atoms/LoadingIndicator.tsx
@@ -1,14 +1,20 @@
+import cx from "classnames";
+
 interface Props {
+    message?: string;
     progress?: {
         value: number;
         max: number;
     };
     isPercentage?: boolean;
+    isFullscreen?: boolean;
 }
 
 export const LoadingIndicator: React.FC<Props> = ({
+    message,
     progress,
     isPercentage,
+    isFullscreen,
 }) => {
     const progressLabel =
         progress && isPercentage
@@ -18,31 +24,42 @@ export const LoadingIndicator: React.FC<Props> = ({
             : null;
 
     return (
-        <div className="w-[50px] aspect-square m-2 relative">
-            <div className="w-full h-full absolute">
-                <div
-                    className="w-full h-full bg-red-500 rounded-full animate-spin absolute"
-                    style={{ transformOrigin: 23 }}
-                />
+        <div
+            className={cx({
+                "w-screen h-screen theme-page-background flex flex-col items-center justify-center absolute top-0 left-0 z-20":
+                    isFullscreen,
+            })}
+        >
+            <div className="w-[50px] aspect-square m-2 relative">
+                <div className="w-full h-full absolute">
+                    <div
+                        className="w-full h-full bg-red-500 rounded-full animate-spin absolute"
+                        style={{ transformOrigin: 23 }}
+                    />
+                </div>
+
+                <div className="w-full h-full rotate-[120deg] absolute">
+                    <div
+                        className="w-full h-full bg-blue-500 rounded-full animate-spin absolute"
+                        style={{ transformOrigin: 23 }}
+                    />
+                </div>
+
+                <div className="w-full h-full rotate-[240deg] absolute">
+                    <div
+                        className="w-full h-full bg-green-500 rounded-full animate-spin absolute"
+                        style={{ transformOrigin: 23 }}
+                    />
+                </div>
+
+                <div className="w-full h-full theme-page-background rounded-full text-opacity-80 flex items-center justify-center absolute">
+                    {progressLabel}
+                </div>
             </div>
 
-            <div className="w-full h-full rotate-[120deg] absolute">
-                <div
-                    className="w-full h-full bg-blue-500 rounded-full animate-spin absolute"
-                    style={{ transformOrigin: 23 }}
-                />
-            </div>
-
-            <div className="w-full h-full rotate-[240deg] absolute">
-                <div
-                    className="w-full h-full bg-green-500 rounded-full animate-spin absolute"
-                    style={{ transformOrigin: 23 }}
-                />
-            </div>
-
-            <div className="w-full h-full theme-page-background rounded-full text-opacity-80 flex items-center justify-center absolute">
-                {progressLabel}
-            </div>
+            {message && isFullscreen && (
+                <p className="text-xs accent-foreground mt-2">{message}</p>
+            )}
         </div>
     );
 };
